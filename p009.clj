de3983615c7a44a47b6468ce4375048de1e60ca7
ntalbs <ntalbsen@gmail.com>
2013-07-13 07:18:39 +0900

    solution of #9

---
+++ p009.clj
@@ -0,0 +1,15 @@
+;; #009
+;; There exists Pythagorean triplet for which a+b+c=1000.
+;; Find the product abc.
+
+; initial: fairly fast when a+b+c is small, but not usable when a+b+c is large.
+(def p009
+  (first (for [a (range 1 1000)
+               b (range a 1000)
+               c [(- 1000 a b)]
+               :when (= (+ (* a a) (* b b)) (* c c))]
+           (* a b c))))
+
+(time (println p009))
+
+; TODO: improved
