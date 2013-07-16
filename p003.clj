ae2587f55fe409b85999c43f8167d3e3a207f764
ntalbs <ntalbsen@gmail.com>
2013-07-16 19:04:39 +0900

    move factorize function to util.clj.

--- p003.clj
+++ p003.clj
@@ -1,19 +1,8 @@
 ;; #003
 ;; largest prime factor of 600851475143
+(use '[util :only [factorize]])
 
 (def N 600851475143)
-
-(defn factorize [n]
-  (let [bound (Math/sqrt n)
-        fact (fn [m i acc]
-               (if (< i bound)
-                 (if (= 0 (mod m i))
-                   (recur (quot m i) i (conj acc i))
-                   (recur m (inc i) acc))
-                 acc))]
-    (->> (fact n 2 '())
-         (group-by identity)
-         (map (fn [[k c]] [k (count c)])))))
 
 (def p003
   (->> (factorize N)
--- util.clj
+++ util.clj
@@ -14,3 +14,15 @@
             (if (< n 10) (conj acc n)
                 (recur (quot n 10) (conj acc (mod n 10)))))]
     (digits-acc n '())))
+
+(defn factorize [n]
+  (let [bound (Math/sqrt n)
+        fact (fn [m i acc]
+               (if (< i bound)
+                 (if (= 0 (mod m i))
+                   (recur (quot m i) i (conj acc i))
+                   (recur m (inc i) acc))
+                 acc))]
+    (->> (fact n 2 '())
+         (group-by identity)
+         (map (fn [[k c]] [k (count c)])))))
