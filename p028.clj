725c3700e6347548f4d85a3c9b14655f04412672
ntalbs <ntalbsen@gmail.com>
2013-07-22 12:55:52 +0900

    Solution of #28.

---
+++ p028.clj
@@ -0,0 +1,16 @@
+;; #028
+
+(defn square [n] (* n n))
+
+; n=(1 2 3 4)라 할 때,
+; i번째 테두리의 대각선 요소는 다음 식으로 구할 수 있음 (i >= 2)
+; l(i) = (2i-2)n + (2i-3)^2
+(defn l [i n]
+  (+ (-> (* 2 i) (- 2) (* n)) (square (-> (* 2 i) (- 3)))))
+
+(defn p028 [size]
+  (let [s (for [i (range 2 (inc (/ (inc size) 2)))]
+            (apply + (map (partial l i) (range 1 5))))]
+    (+ 1 (apply + s))))
+
+(time (println (p028 1001)))
