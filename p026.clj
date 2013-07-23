3548dfb115c6a6c85bd35443f70cdd047f5a1276
ntalbs <ntalbsen@gmail.com>
2013-07-24 07:27:18 +0900

    Solution of #26.

---
+++ p026.clj
@@ -0,0 +1,21 @@
+;; #026
+;; Find the value of d  1000 for which 1/d contains the longest recurring cycle
+;; in its decimal fraction part.
+
+(defn qs [n]
+  "Returns the unit fraction of denominator n 
+   in the form of [n [quotient digits] (# of digits of recurring cycle)].
+   Though the second element of the vector also counts non recurring part,
+   it doesn't affect the answer."
+  (loop [r 10 r-acc #{} q-acc []]
+    (cond (zero? r) [n q-acc 0]                       ; no recurring cycle
+          (contains? r-acc r) [n q-acc (count q-acc)] ; recurring
+          :else (recur (* 10 (rem r n)) (conj r-acc r) (conj q-acc (quot r n))))))
+
+(defn p026 []
+  (->> (range 1 (inc 1000))
+       (map (fn [n] (qs n)))
+       (apply max-key (fn [[n _ cnt]] cnt))
+       first))
+
+(time (println (p026)))
