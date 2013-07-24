97c84f11da188b77b943d8118ce3356f7dcc05e0
ntalbs <ntalbsen@gmail.com>
2013-07-25 08:18:51 +0900

    Solution of #34.

---
+++ p034.clj
@@ -0,0 +1,22 @@
+;; #034
+;; Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+(use '[util :only (digits factorial)])
+
+(defn check [n]
+  (= n
+     (->> (digits n)
+          (map factorial)
+          (apply +))))
+
+; Don't know the upper limit, used brute force attack.
+; First, find the smallest one (X1) that satisfied the condition.
+; Then try to the next one (X2) and them.
+; Luckly, that's the answer.
+(defn p034 []
+  (->> (iterate inc 11)
+       (filter check)
+       (take 2)
+       (apply +)))
+
+(time (println (p034)))
