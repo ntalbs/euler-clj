9249b80f2412091c42c30af8a1d9653507aa7a44
ntalbs <ntalbsen@gmail.com>
2013-08-01 18:47:33 +0900

    Add project.clj to version control.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject euler "0.1.0-SNAPSHOT"
+  :description "Project Euler solutions in Clojure"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojure/math.combinatorics "0.0.4"]])
