8330809d7e61c1571488843c6afc0cd9c8ef81af
ntalbs <ntalbsen@gmail.com>
2013-08-04 10:45:36 +0900

    change p007 to function.

--- src/p007.clj
+++ src/p007.clj
@@ -1,6 +1,6 @@
 ;; #007
 (use '[util :only (prime?)])
 
-(def p007 (last (take 10001 (filter prime? (iterate inc 2)))))
+(defn p007 [] (last (take 10001 (filter prime? (iterate inc 2)))))
 
-(time (println p007))
+(time (println (p007)))
