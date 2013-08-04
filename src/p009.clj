8f2095fc516346466113dec5416fe8cbcf1b7d60
ntalbs <ntalbsen@gmail.com>
2013-08-04 16:30:38 +0900

    change p009 to function.

--- src/p009.clj
+++ src/p009.clj
@@ -3,13 +3,13 @@
 ;; Find the product abc.
 
 ; initial: fairly fast when a+b+c is small, but not usable when a+b+c is large.
-(def p009
+(defn p009 []
   (first (for [a (range 1 1000)
                b (range a 1000)
                c [(- 1000 a b)]
                :when (= (+ (* a a) (* b b)) (* c c))]
            (* a b c))))
 
-(time (println p009))
+(time (println (p009)))
 
 ; improved: TODO
