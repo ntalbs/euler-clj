e7889d302d3c2c62bdc56d5c7187d4196809b304
ntalbs <ntalbsen@gmail.com>
2013-08-07 18:51:50 +0900

    Solution of #42.

---
+++ src/p042.clj
@@ -0,0 +1,24 @@
+;; #042
+
+(defn t [n]
+  (-> (+ n 1) (* n) (/ 2)))
+
+(def t-set
+  (into #{} (map t (range 1 100))))
+
+(defn word-value [word]
+  (->> (map #(- (int %) 64) word)
+       (apply +)))
+
+(defn triangle-number? [n]
+  (contains? t-set n))
+
+(def words
+  (clojure.string/split (slurp "data/words.txt") #"\"(,\")?"))
+
+(defn p042 []
+  (->> (map word-value words)
+       (filter triangle-number?)
+       count))
+
+(time (println (p042)))
