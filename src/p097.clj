3a457609b0d19d52179d9a926dca2dff04b688bd
ntalbs <ntalbsen@gmail.com>
2013-08-11 22:17:40 +0900

    Solution of #97.

---
+++ src/p097.clj
@@ -0,0 +1,10 @@
+;; #097
+;; Find the last ten digits of 28433×2^7830457+1.
+
+(defn p097 []
+  (-> (.modPow (biginteger 2) (biginteger 7830457) (biginteger 10000000000))
+      (.multiply (biginteger 28433))
+      (.add (biginteger 1))
+      (.mod (biginteger 10000000000))))
+
+(time (println (p097)))
