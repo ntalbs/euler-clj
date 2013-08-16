d2bdb59fe4665c3dc5eee61f520bfa719e454b48
ntalbs <ntalbsen@gmail.com>
2013-08-16 18:44:14 +0900

    Update p099.clj.

--- src/p099.clj
+++ src/p099.clj
@@ -2,15 +2,15 @@
 
 (use '[clojure.string :only (split)])
 
-(defn parseInt [s] (Integer/parseInt s))
+(defn parse-int [s] (Integer/parseInt s))
 
 (def log-vals
   (->> (map (fn [s] (split s #",")) (split (slurp "data/base_exp.txt") #"\r\n"))
-       (map (fn [[b e]] [(parseInt b) (parseInt e)]))
+       (map (fn [[b e]] [(parse-int b) (parse-int e)]))
        (map (fn [[b e]] (* e (Math/log10 b))))
        (map-indexed vector)))
 
 (defn p099 []
-  (+ 1 (apply max-key (fn [[i v]] v) log-vals))) ; +1, since log-vals index is zero-based
+  (+ 1 (first (apply max-key (fn [[i v]] v) log-vals)))) ; +1, since log-vals index is zero-based
 
 (time (println (p099)))
