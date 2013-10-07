c33187ccab8f204704da93c7ddba91496a338a78
ntalbs <ntalbsen@gmail.com>
2013-10-08 07:34:11 +0900

    Add comments.

--- src/p075.clj
+++ src/p075.clj
@@ -2,18 +2,18 @@
 
 (use '[util :only [gcd]])
 
-(def l-max 1500000)
+(def l-max 1500000)                     ;maximum length of wire
 (def limit (int (Math/sqrt l-max)))
 
 (defn p075 []
   (->> (for [m (range 1 (inc limit)) n (range 1 m)
              :when (odd? (- m n))
              :when (= 1 (gcd m n))]
-         (sort [(- (* m m) (* n n)) (* 2 m n) (+ (* m m) (* n n))]))
+         (sort [(- (* m m) (* n n)) (* 2 m n) (+ (* m m) (* n n))]))      ; primitive triples
        (mapcat (fn [[a b c]]
                  (->> (iterate inc 1)
                       (map (fn [k] [(* k a) (* k b) (* k c)]))
-                      (take-while (fn [[a b c]] (<= (+ a b c) l-max))))))
+                      (take-while (fn [[a b c]] (<= (+ a b c) l-max)))))) ; triples, all
        (filter (fn [[a b c]] (<= (+ a b c) l-max)))
        (group-by (fn [[a b c]] (+ a b c)))
        (filter (fn [[_ list]] (= 1 (count list))))
