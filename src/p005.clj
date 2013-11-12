b577ebd5112f1fc203e7497da4369dedbc48bd93
ntalbs <ntalbsen@gmail.com>
2013-11-12 11:04:11 +0900

    Update p005.clj

--- src/p005.clj
+++ src/p005.clj
@@ -1,8 +1,8 @@
 ;; #005
-;; smallest positive number that is evenly divisible 
+;; smallest positive number that is evenly divisible
 ;; by all of the numbers from 1 to 20
 
-(use '[util :only (gcd lcm)])
+(use '[util :only (lcm)])
 
 (defn p005 []
   (reduce lcm (range 1 21)))
