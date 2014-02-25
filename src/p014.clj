83996401cb81590df40e23a4a785e9e5f82ce1cb
ntalbs <ntalbsen@gmail.com>
2014-02-26 08:40:07 +0900

    modify not to use `letfn`

--- src/p014.clj
+++ src/p014.clj
@@ -1,13 +1,13 @@
 ;; #014
 
 (defn x [n]
-  (letfn [(x [n acc]
-            (if (= 1 n)
-              (conj acc 1)
-              (if (even? n)
-                (recur (quot n 2) (conj acc n))
-                (recur (+ (* 3 n) 1) (conj acc n)))))]
-    (x n [])))
+  (loop [n n acc []]
+    (if (= 1 n)
+      (conj acc 1)
+      (if (even? n)
+        (recur (quot n 2) (conj acc n))
+        (recur (+ (* 3 n) 1) (conj acc n))))))
+
 
 (defn p014 []
   (->> (range 1 (inc 1000000))
