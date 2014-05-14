d59fc83cc17d538cdd07a223ab7aebfc1a06b2a4
ntalbs <ntalbsen@gmail.com>
2014-05-14 12:24:10 +0900

    cider-nrepl 플러그인 추가

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Project Euler solutions in Clojure"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/math.combinatorics "0.0.4"]])
+                 [org.clojure/math.combinatorics "0.0.4"]]
+  :plugins [[cider/cider-nrepl "0.6.0"]]
+)
