51ca3aeccce5e46ddc503421aba63275a0ead3db
ntalbs <ntalbsen@gmail.com>
2014-05-19 09:24:12 +0900

    clojure 1.6으로 변경.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler "0.1.0-SNAPSHOT"
   :description "Project Euler solutions in Clojure"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.combinatorics "0.0.4"]]
   :plugins [[cider/cider-nrepl "0.6.0"]]
