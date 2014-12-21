2a286ba1ef1c157baeb10c502a6fa61deae9a427
ntalbs <ntalbsen@gmail.com>
2014-12-21 09:53:29 +0900

    Solution of #95. Slow. Need to be improved.

---
+++ src/p095.clj
@@ -0,0 +1,29 @@
+;; slow. need to be improved.
+
+(ns p095
+  (:require [util :refer [proper-divisors]]))
+
+(def limit 1000000)
+
+(defn sum-of-divs [n]
+  (cond (nil? n) nil
+        (= n 1) nil
+        (> n limit) nil
+        :else (apply + (proper-divisors n))))
+
+(def sum-of-divs (memoize sum-of-divs))
+
+(defn find-chain [n]
+  (loop [n n s #{} acc []]
+    (if-not (contains? s n)
+      (recur (sum-of-divs n) (conj s n) (conj acc n))
+      (if (or (nil? (last acc)) (not= n (first acc))) nil acc))))
+
+(defn longest-chain [limit]
+  (->> (range 2 (inc limit))
+    (map find-chain)
+    (remove nil?)
+    (apply max-key count)))
+
+(defn solve []
+  (time (println (apply min (longest-chain limit)))))
