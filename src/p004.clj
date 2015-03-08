72c40e690cca182e9eac3ab85e661f134f454460
ntalbs <ntalbsen@gmail.com>
2015-03-08 22:30:41 +0900

    함수 이름 변경.

--- src/p004.clj
+++ src/p004.clj
@@ -3,28 +3,29 @@
 
 (ns p004)
 
+(def limit 1000)
+
 (defn palindrome? [n]
   (= (str n) (apply str (reverse (str n)))))
 
 ; initial
-(defn p004-1 []
-  (->> (for [a (range 100 1000) b (range a 1000)] (* a b))
+(defn initial-approach []
+  (->> (for [a (range 100 limit) b (range a limit)] (* a b))
        (filter palindrome?)
        (apply max)))
-
 
 ; improved
 ; P = ab = 100000x + 10000y + 1000z + 100z + 10y + x
 ;        = 100001x + 10010y + 1100z
 ;        = 11(9091x + 910y + 100z)
 ; 11 is prime, a or b must have a factor of 11
-(defn p004-2 []
-  (->> (for [a (range 100 1000) b (range a 1000)
+(defn improved []
+  (->> (for [a (range 100 limit) b (range a limit)
              :when (or (= 0 (mod a 11)) (= 0 (mod b 11)))]
          (* a b))
        (filter palindrome?)
        (apply max)))
 
 (defn solve []
-  (time (println "initial => " (p004-1)))
-  (time (println "improved => " (p004-2))))
+  (time (println "initial => " (initial-approach)))
+  (time (println "improved => " (improved))))
