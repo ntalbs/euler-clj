87f2f2fe44ab1fa6f6e18d380da93bfa9dd03fdf
ntalbs <ntalbsen@gmail.com>
2015-04-06 09:51:15 +0900

    p### 함수 제거. 실제 풀이 함수를 solve로 변경. factorial 함수 직접 사용하도록 수정.

--- src/p015.clj
+++ src/p015.clj
@@ -4,11 +4,6 @@
 (ns p015
   (:require [util :refer [factorial]]))
 
-(def f factorial)
-
-(defn p015 []
-  (let [f40 (f 40) f20 (f 20)]
-    (/ f40 (*' f20 f20))))
-
 (defn solve []
-  (time (println (p015))))
+  (let [f40 (factorial 40) f20 (factorial 20)]
+    (/ f40 (*' f20 f20))))
