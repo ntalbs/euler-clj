e58a3b9e1a4ae55e2c2e892269e941c65f85aab7
ntalbs <ntalbsen@gmail.com>
2015-04-15 09:45:02 +0900

    리팩터링 (기본 로직은 동일)

--- src/p019.clj
+++ src/p019.clj
@@ -1,17 +1,11 @@
 (ns p019
   (:import java.util.Calendar))
 
-(def cal (Calendar/getInstance))
+(defn solve1 []
+  (let [cal (Calendar/getInstance)]
+    (count (for [year (range 1901 2001)
+                 month (range 0 12)
+                 :when (= Calendar/SUNDAY (do (. cal set year month 1)
+                                              (. cal get Calendar/DAY_OF_WEEK)))]
+             [year month 1]))))
 
-(def sundays-fell-on-the-first-of-the-month-during-the-20th-century
-  (for [year (range 1901 2001) month (range 0 12)
-        :when (= Calendar/SUNDAY (do
-                                   (. cal set year month 1)
-                                   (. cal get Calendar/DAY_OF_WEEK)))]
-         [year month 1]))
-
-(defn p019 []
-  (count sundays-fell-on-the-first-of-the-month-during-the-20th-century))
-
-(defn solve []
-  (time (println (p019))))
