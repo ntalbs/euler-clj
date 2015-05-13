9fb453c43a27d92bbef6ff99bd8e39131b45dcd4
ntalbs <ntalbsen@gmail.com>
2015-05-13 09:26:03 +0900

    날짜 라이브러리를 사용하지 않은 풀이 추가.

--- src/p019.clj
+++ src/p019.clj
@@ -17,3 +17,29 @@
          (take-while #(<= (.getYear %) 2000))
          (filter #(= (.getDayOfWeek %) DayOfWeek/SUNDAY))
          (count))))
+
+(defn leap-year? [year]
+  (if (zero? (mod year 100))
+    (zero? (mod year 400))
+    (zero? (mod year 4))))
+
+(defn days-in-month [year month]
+  (cond (#{1 3 5 7 8 10 12} month) 31
+        (#{4 6 9 11} month) 30
+        (leap-year? year) 29
+        :else 28))
+
+(defn next-date [[year month dm dw]]
+  (let [last-day (days-in-month year month)]
+    (if (< dm last-day)
+      [year month (inc dm) (mod (inc dw) 7)]
+      (if (< month 12)
+        [year (inc month) 1 (mod (inc dw) 7)]
+        [(inc year) 1 1 (mod (inc dw) 7)]))))
+
+(defn solve3 []
+  (->> (iterate next-date [1900 1 1 1])
+       (drop-while (fn [[year _ _ _]] (<= year 1900)))
+       (take-while (fn [[year _ _ _]] (<= year 2000)))
+       (filter (fn [[_ _ dm dw]] (and (= 1 dm) (zero? dw))))
+       (count)))
