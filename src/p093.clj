f9d084d03c8a80c7fe1865abf421ac6f192fcf2c
ntalbs <ntalbsen@gmail.com>
2015-05-27 11:27:08 +0900

    private 함수로 변경.

--- src/p093.clj
+++ src/p093.clj
@@ -3,14 +3,14 @@
 
 ;; 일단 답은 구하지만 target 함수가 정말 모든 경우를 고려하고 있는지는 검토가 필요함.
 
-(defn target [[a b c d]]
+(defn- target [[a b c d]]
   (let [op [+ - * /]]
     (->> (for [f1 op, f2 op, f3 op]
            [(-> (f1 a b) (f2 c) (f3 d))
             (-> (f3 (f1 a b) (f2 c d)))])
          (flatten))))
 
-(defn gen [[a b c d]]
+(defn- gen [[a b c d]]
   (->> (permutations [a b c d])
        (mapcat #(target %))
        (distinct)
@@ -18,7 +18,7 @@
        (sort)
        (hash-map :set [a b c d] :target)))
 
-(defn count-consective [ns]
+(defn- count-consective [ns]
   (->> (partition 2 1 ns)
        (take-while (fn [[a b]] (= a (dec b))))
        (count)
