6d820bed7404299397b13044f6c22c8969af95dd
ntalbs <ntalbsen@gmail.com>
2015-06-09 10:39:03 +0900

    미사용 함수 require 목록에서 삭제.

--- src/p021.clj
+++ src/p021.clj
@@ -1,7 +1,7 @@
 ;; Evaluate the sum of all the amicable numbers under 10000.
 
 (ns p021
-  (:require [util :refer [divisor? sum-of-proper-divisor]]))
+  (:require [util :refer [sum-of-proper-divisor]]))
 
 (defn amicable [a]
   (let [b (sum-of-proper-divisor a)]
@@ -9,8 +9,5 @@
       a
       0)))
 
-(defn p021 []
-  (apply + (map amicable (range 1 10000))))
-
 (defn solve []
-  (time (println (p021))))
+  (apply + (map amicable (range 1 10000))))
