4c8a1b7701733fdf053c68173222d5a61b6dccad
ntalbs <ntalbsen@gmail.com>
2015-06-20 14:16:25 +0900

    풀이 추가.

--- src/p021.clj
+++ src/p021.clj
@@ -1,11 +1,14 @@
 ;; Evaluate the sum of all the amicable numbers under 10000.
 
 (ns p021
-  (:require [util :refer [sum-of-proper-divisor]]))
+  (:require [util :refer [aliquot-sum aliquot-sum-by-formula]]))
 
-(defn amicable? [a]
-  (let [b (sum-of-proper-divisor a)]
-    (and (not= a b) (= a (sum-of-proper-divisor b)))))
+(defn amicable? [a f]
+  (let [b (f a)]
+    (and (not= a b) (= a (f b)))))
 
-(defn solve []
-  (apply + (filter amicable? (range 1 10000))))
+(defn solve1 []
+  (apply + (filter #(amicable? % aliquot-sum) (range 1 10000))))
+
+(defn solve2 []
+  (apply + (filter #(amicable? % aliquot-sum-by-formula) (range 1 10000))))
