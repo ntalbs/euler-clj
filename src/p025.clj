20c031d1dc22b4dc3748f3f4ea6405156a660b3c
ntalbs <ntalbsen@gmail.com>
2015-07-09 14:24:56 +0900

    함수 이름/구조 변경.

--- src/p025.clj
+++ src/p025.clj
@@ -4,12 +4,8 @@
 
 (defn fibo [[a b]] [b (+' a b)])
 
-(defn p025 []
+(defn solve []
   (->> (iterate fibo [1 1])
        (map-indexed (fn [i e] [(inc i) e]))
        (drop-while (fn [[_ [a b]]] (< (count (str a)) 1000)))
-       first
-       first))
-
-(defn solve []
-  (time (println (p025))))
+       ffirst))
