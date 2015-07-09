e858edb8c90bdc73a1e396b7da81e4a13a3b18e8
ntalbs <ntalbsen@gmail.com>
2015-07-09 14:24:36 +0900

    함수 이름/구조 변경.

--- src/p024.clj
+++ src/p024.clj
@@ -2,10 +2,8 @@
 ;; the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 (ns p024
-  (:require [clojure.math.combinatorics :refer [permutations]]))
+  (:require [clojure.math.combinatorics :as c]))
 
-(defn p024 []
-  (apply str (nth (permutations (range 10)) (dec 1000000))))
+(defn solve1 []
+  (apply str (nth (c/permutations (range 10)) (dec 1000000))))
 
-(defn solve []
-  (time (println (p024))))
