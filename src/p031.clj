301ac8a00a207f7b65d72bcc759358e0c304f41f
ntalbs <ntalbsen@gmail.com>
2015-09-22 08:28:56 +0900

    함수, 파라미터 이름 변경. cond를 case로 변경.

--- src/p031.clj
+++ src/p031.clj
@@ -2,15 +2,16 @@
 
 (ns p031)
 
-(defn first-denomination [kinds-of-coins]
-  (cond (= kinds-of-coins 1) 1
-        (= kinds-of-coins 2) 2
-        (= kinds-of-coins 3) 5
-        (= kinds-of-coins 4) 10
-        (= kinds-of-coins 5) 20
-        (= kinds-of-coins 6) 50
-        (= kinds-of-coins 7) 100
-        (= kinds-of-coins 8) 200))
+(defn- value [coin]
+  (case coin
+    1 1
+    2 2
+    3 5
+    4 10
+    5 20
+    6 50
+    7 100
+    8 200))
 
 (defn cc [amount kinds-of-coins]
   (cond (= amount 0) 1
