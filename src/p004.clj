40268cf6c5bf156bce73959ebcacfdda9770c89d
ntalbs <ntalbsen@gmail.com>
2015-10-06 09:08:54 +0900

    주석 형식 변경

--- src/p004.clj
+++ src/p004.clj
@@ -14,11 +14,11 @@
        (filter palindrome?)
        (apply max)))
 
-; improved
-; P = ab = 100000x + 10000y + 1000z + 100z + 10y + x
-;        = 100001x + 10010y + 1100z
-;        = 11(9091x + 910y + 100z)
-; 11 is prime, a or b must have a factor of 11
+;; improved
+;; P = ab = 100000x + 10000y + 1000z + 100z + 10y + x
+;;        = 100001x + 10010y + 1100z
+;;        = 11(9091x + 910y + 100z)
+;; 11 is prime, a or b must have a factor of 11
 (defn solve2 []
   (->> (for [a (range 100 limit) b (range a limit)
              :when (or (= 0 (mod a 11)) (= 0 (mod b 11)))]
