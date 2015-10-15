c60b8457f11f050dcd504eb57332a8b3a5354da4
ntalbs <ntalbsen@gmail.com>
2015-10-15 23:04:03 +0900

    십진수=>이진수 변환 함수 구현 추가

--- src/p036.clj
+++ src/p036.clj
@@ -1,22 +1,26 @@
 (ns p036
   (:require [clojure.string :as s]))
 
-(defn binstr [n]
-  (Integer/toBinaryString n))
+(defn to-binary-str [n]
+  {:pre [(<= 0 n)]}
+  (loop [n n acc '()]
+    (if (< 0 n)
+      (recur (quot n 2) (conj acc (rem n 2)))
+      (if (empty? acc)
+        "0"
+        (apply str acc)))))
 
-(defn palindromic? [s]
+(defn- palindromic? [s]
   (= s (s/reverse s)))
 
-(defn dec-palindromic? [n]
+(defn- dec-palindromic? [n]
   (palindromic? (str n)))
 
-(defn bin-palindromic? [n]
+(defn- bin-palindromic? [n]
+;;  (palindromic? (to-binary-str n)))
   (palindromic? (Integer/toBinaryString n)))
 
-(defn p036 []
+(defn solve []
   (->> (range 1 (inc 1000000))
        (filter (fn [n] (and (dec-palindromic? n) (bin-palindromic? n))))
        (apply +)))
-
-(defn solve []
-  (time (println (p036))))
