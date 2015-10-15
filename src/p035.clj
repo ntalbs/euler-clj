379607d131c123432c295cf4aa31d98256c4d790
ntalbs <ntalbsen@gmail.com>
2015-10-15 23:02:59 +0900

    풀이 추가

--- src/p035.clj
+++ src/p035.clj
@@ -1,24 +1,51 @@
-;; How many circular primes are there below one million?
-
 (ns p035
-  (:require [util :refer [parse-int digits prime?]]
-            [clojure.contrib.lazy-seqs :refer [primes]]))
+  (:require [clojure.contrib.lazy-seqs :refer [primes]]
+            [util :refer [parse-int digits prime? pow]]))
 
-(defn circular-nums [n]
+(defn- circular-nums [n]
   (let [ds (digits n) len (count ds)]
     (->> (cycle ds)
          (partition len 1)
          (take len)
          (map #(parse-int (apply str %))))))
 
-(defn all-prime? [coll]
+(defn- all-prime? [coll]
   (every? prime? coll))
 
-(defn p035 []
+(defn solve0 []
   (->> primes
        (take-while #(< % 1000000))
        (filter (fn [n] (all-prime? (circular-nums n))))
        count))
 
-(defn solve []
-  (time (println (p035))))
+(defn- cycle1 [n p]
+  (let [i (quot n 10) l (rem n 10)]
+    (+ (* l (pow 10 p)) i)))
+
+(defn- circular-prime1? [n]
+  (let [p (int (Math/log10 n))]
+    (loop [n n, cnt p]
+      (cond
+        (= 0 cnt) (prime? n)
+        (prime? n) (recur (cycle1 n p) (dec cnt))
+        :else false))))
+
+(defn- circular-prime2? [n] ;; 처음 전달된 n은 prime이라고 가정
+  (let [p (int (Math/log10 n))]
+    (loop [n (cycle1 n p), cnt (dec p)]
+      (cond
+        (<= cnt 0) (prime? n)
+        (prime? n) (recur (cycle1 n p) (dec cnt))
+        :else false))))
+
+(defn solve1 []
+  (->> primes
+       (take-while #(< % 1000000))
+       (filter circular-prime?)
+       count))
+
+(defn solve2 []
+  (->> primes
+       (take-while #(< % 1000000))
+       (filter circular-prime2?)
+       count))
