e74efff9e14c7263d8ab130fd9def1737020dd90
ntalbs <ntalbsen@gmail.com>
2015-10-21 09:28:25 +0900

    주석 삭제.

--- src/p038.clj
+++ src/p038.clj
@@ -1,7 +1,3 @@
-;; What is the largest 1 to 9 pandigital 9-digit number
-;; that can be formed as the concatenated product of an integer
-;; with (1,2, ... , n) where n > 1?
-
 (ns p038)
 
 (defn pandigital? [sn]
@@ -13,9 +9,6 @@
       (apply str acc)
       (recur (inc i) (concat acc (str (* n i)))))))
 
-; The number of digits of multiplicand(M) should be less than or equal to 4.
-; M x 1 = dddd
-; M x 2 = ddddd
 
 (defn solve []
   (->> (range 9999 1 -1)
