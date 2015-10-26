41e80d2a1e275c5a51747034d422d89c97fea206
ntalbs <ntalbsen@gmail.com>
2015-10-26 17:14:49 +0900

    미사용 코드 삭제.

--- src/p040.clj
+++ src/p040.clj
@@ -3,14 +3,8 @@
 
 (def ds (mapcat digits (iterate inc 1)))
 
-(defn d [n]
-  (nth ds n))
-
-(defn p040[]
+(defn solve []
   (->> (range 7)
        (map #(pow 10 %))
        (map #(nth ds (dec %)))
        (apply *)))
-
-(defn solve []
-  (time (println (p040))))
