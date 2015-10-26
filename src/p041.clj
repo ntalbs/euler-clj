f71aac1064b0e83d6d8a9c3cf00daef8c80507e0
ntalbs <ntalbsen@gmail.com>
2015-10-26 17:15:01 +0900

    리팩터링.

--- src/p041.clj
+++ src/p041.clj
@@ -2,20 +2,10 @@
   (:require [util :refer [parse-int prime?]]
             [clojure.contrib.combinatorics :refer [permutations]]))
 
-(def ds (range 1 10))
-
-(defn investigate [v]
-  (->> (permutations (reverse v))
+(defn solve []
+  (->> (reverse (reductions conj [1] (range 2 10)))
+       (map reverse)
+       (mapcat permutations)
        (map #(parse-int (apply str %)))
        (filter prime?)
-       (take 1)))
-
-(defn p041 []
-  (flatten (->> (range 9 0 -1)
-                (map #(range 1 (inc %)))
-                (map investigate)
-                (drop-while empty?)
-                first)))
-
-(defn solve []
-  (time (println (p041))))
+       first))
