6e4450b75ec2dd6c4ee07dcc1dbfea70dc691a2d
ntalbs <ntalbsen@gmail.com>
2015-10-26 17:14:35 +0900

    리팩터링.

--- src/p039.clj
+++ src/p039.clj
@@ -1,25 +1,23 @@
-;; Refer to http://en.wikipedia.org/wiki/Pythagorean_triple
-
 (ns p039
   (:require [util :refer (gcd)]))
 
-(defn triple
+(def limit 1000)
+
+(defn- triple
   "Returns a pythagorian triple for given m, n with m > n."
   [m n k]
   [(* k (- (* m m) (* n n)))
    (* k 2 m n)
    (* k (+ (* m m) (* n n)))])
 
-(defn p039 [bound]
-  (->> (for [m (range 1 (int (Math/sqrt bound)))
+(defn solve []
+  (->> (for [m (range 2 (int (Math/sqrt (/ limit 4))))
              n (range 1 m)
-             k (range 1 (int (/ 1000 12)))
+             k (range 1 (quot limit 12))
              :when (odd? (- m n))
              :when (= 1 (gcd m n))]
          (triple m n k))
-       (filter (fn [[a b c]] (<= (+ a b c) bound)))
+       (filter (fn [[a b c]] (<= (+ a b c) limit)))
        (group-by (fn [[a b c]] (+ a b c)))
-       (apply max-key (fn [[p v]] (count v)))))
-
-(defn solve []
-  (time (println (p039 1000))))
+       (apply max-key (fn [[p v]] (count v)))
+       first))
