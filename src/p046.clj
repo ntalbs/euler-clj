cdb997d405a0d662c39797bbd47ac287d03913d0
ntalbs <ntalbsen@gmail.com>
2016-01-14 08:01:14 +0000

    코드 수정

--- src/p046.clj
+++ src/p046.clj
@@ -1,19 +1,17 @@
 (ns p046
   (:require [clojure.contrib.lazy-seqs :refer [primes]]
-            [clojure.set :refer [difference]]
-            [util :refer [prime?]]))
+            [clojure.set :refer [difference]]))
 
 (def limit 10000)
 
 (def ps (take-while #(< % limit) primes))
 
 (def odd-composite-nums
-  (->> (range 3 limit 2)
-       (filter #(not (prime? %)))
-       (into #{})))
+  (difference (set (range 3 limit 2))
+              ps))
 
 (def goldbach-nums
-  (into #{} (for [p ps n (range 1 (Math/sqrt limit))] (+ p (* 2 n n)))))
+  (set (for [p ps n (range 1 (Math/sqrt limit))] (+ p (* 2 n n)))))
 
 (defn solve []
   (apply min (difference odd-composite-nums goldbach-nums)))
