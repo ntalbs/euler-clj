50b896ab60992dd2bb8b02a3ac12fd28e95d5c4a
ntalbs <ntalbsen@gmail.com>
2016-01-22 16:43:29 +0000

    동일 계산을 반복하지 않도록 수정.

--- src/p047.clj
+++ src/p047.clj
@@ -5,7 +5,8 @@
   (count (factorize n)))
 
 (defn solve []
-  (->> (iterate inc 1)
+  (->> (iterate inc 2)
+       (map (fn [n] [n (count-prime-factors n)]))
        (partition 4 1)
-       (filter (fn [ns] (every? #(= 4 %) (map count-prime-factors1 ns))))
-       ffirst))
+       (filter (fn [ns] (every? #(= 4 %) (map (fn [[_ c]] c) ns))))
+       first first first))
