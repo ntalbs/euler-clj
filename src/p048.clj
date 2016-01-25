8f8fce6d2bfb7bed98e7f56533497696c527af5c
ntalbs <ntalbsen@gmail.com>
2016-01-25 08:55:25 +0000

    코드 정리

--- src/p048.clj
+++ src/p048.clj
@@ -1,11 +1,8 @@
 (ns p048)
 
-(defn p048 []
-  (.mod (->> (range 1 (inc 1000))
-             (map biginteger)
-             (map #(.pow % %))
-             (apply +')
-             (.toBigInteger)) (biginteger 10000000000)))
-
 (defn solve []
-  (time (println (p048))))
+  (->> (range 1 (inc 1000))
+       (map biginteger)
+       (map #(.pow % %))
+       (apply +')
+       (#(mod % 10000000000))))
