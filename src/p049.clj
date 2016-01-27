a92fc2b844c010f5ff52a1c2691100c77dd28bdd
ntalbs <ntalbsen@gmail.com>
2016-01-27 09:44:53 +0000

    풀이 재구현. 이제 답을 제대로 구한다.

--- src/p049.clj
+++ src/p049.clj
@@ -1,44 +1,34 @@
 (ns p049
   (:require [clojure.contrib.lazy-seqs :refer [primes]]
             [clojure.math.combinatorics :refer [permutations]]
-            [util :refer [parse-int digits prime?]]))
+            [util :refer [parse-int digits]]))
 
 (def four-digits-primes
   (->> primes
        (drop-while #(< % 1000))
-       (take-while #(< % 9999))))
+       (take-while #(< % 9999))
+       set))
 
-(defn permutation-nums [n]
-  (->> (permutations (util/digits n))
-       (map #(parse-int (apply str %)))
-       (filter #(<= n %))
-       (distinct)
-       (sort)))
+(defn permutation-primes [p]
+  (->> (digits p)
+       permutations
+       (map #(apply str %))
+       (map #(parse-int %))
+       (filter four-digits-primes)
+       sort))
 
-(defn check-permutation-primes [p]
-  (->> (permutation-nums p)
-       (filter prime?)
-       (sort)
-       (distinct)))
-
-(defn arithmetic-seq? [v]
-  (->> (partition 2 1 v)
-       (map (fn [[a b]] (- b a)))
-       (distinct)
-       (count)
-       (= 1)))
-
-(defn p049 []
-  (->> four-digits-primes
-       (map check-permutation-primes)
-       (filter #(<= 3 (count %)))
-       (filter arithmetic-seq?)
-       (map #(apply str %))))
-
-; This code find the answer.
-; However, due to the incompleteness of arithmetic-seq? funciton,
-; don't find 1487, 4817, 8147.
-; This is just a luck.
+(defn find-arithmetic-seq [v]
+  (->> (for [i v, j v :when (< i j)] [i j])
+       (group-by (fn [[a b]] (- b a)))
+       (filter (fn [[_ ds]] (= 2 (count ds))))
+       (filter (fn [[_ [[_ a] [b _]]]] (= a b)))
+       (map (fn [[_ [[a b] [c d]]]] [a b d]))
+       first))
 
 (defn solve []
-  (time (println (p049))))
+  (->> four-digits-primes
+       (map permutation-primes)
+       (map find-arithmetic-seq)
+       (filter not-empty)
+       distinct
+       (map #(apply str %))))
