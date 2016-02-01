ef04ef6e20c031d9404d38390aed365436267441
ntalbs <ntalbsen@gmail.com>
2016-02-01 07:40:24 +0000

    코드 정리.

--- src/p050.clj
+++ src/p050.clj
@@ -3,15 +3,13 @@
             [util :refer [prime?]]))
 
 (def csp "cumulative sums of primes"
-  (->> (reductions + primes)
+  (->> primes
+       (reductions +)
        (take-while #(< % 1000000))))
 
-(defn p050 []
+(defn solve []
   (->> (for [s (conj csp 0)] (map #(- % s) csp))
        flatten
        (filter pos?)
        (filter prime?)
        (apply max)))
-
-(defn solve []
-  (time (println (p050))))
