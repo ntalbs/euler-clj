9f46fdcc210b823f9ae7212048a7b779a2ff9c1c
ntalbs <ntalbsen@gmail.com>
2016-02-10 08:56:10 +0000

    가장 길게 연속되는 소수의 합으로 표현되는 소수를 구하도록 수정 기존 풀이는 소수 자체가 최대인 경우를 구하게 되어 있었음 (오류)

--- src/p050.clj
+++ src/p050.clj
@@ -2,13 +2,16 @@
   (:require [clojure.contrib.lazy-seqs :refer [primes]]
             [util :refer [prime?]]))
 
-(def csp "cumulative sums of primes"
+(def csp "indexes and cumulative sums of primes"
   (->> primes
-       (reductions + 0)
-       (take-while #(< % 1000000))))
+       (reductions (fn [a p] [(inc (first a)) (+ (second a) p)]) [0 0])
+       (take-while (fn [[_ s]] (< s 1000000)))))
 
 (defn solve []
-  (->> (for [s csp t csp :when (< s t)] (- t s))
-       (filter odd?)
-       (filter prime?)
-       (apply max)))
+  (->> (for [csp1 csp, csp2 csp
+             :let [[i1 s1] csp1, [i2 s2] csp2]
+             :when (< i1 i2)]
+         [(- i2 i1) (- s2 s1)])
+       (filter (fn [[_ s]] (prime? s)))
+       (apply max-key first)
+       second))
