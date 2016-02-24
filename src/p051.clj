173408a5338dc242e27fa7f544763b39b0ae442e
ntalbs <ntalbsen@gmail.com>
2016-02-24 08:25:39 +0000

    풀이 방식 변경.

--- src/p051.clj
+++ src/p051.clj
@@ -1,47 +1,39 @@
-;; 인터넷에서 답을 찾아보면 6자리수 소수여야 하고, 반복되는 자리는 3개여야 한다는 말이 나오는데,
-;; 왜 그런지 이해가 되지 않음. 그냥 무식하게 다 해본다.
-
 (ns p051
   (:require [clojure.contrib.lazy-seqs :refer [primes]]
-            [clojure.math.combinatorics :refer [permutations]]
-            [util :refer [digits]]))
+            [clojure.string :as s]
+            [util :refer [digits prime?]]))
 
-(def pattern5digits
-  (lazy-cat (permutations [0 0 0 0 1])
-            (permutations [0 0 0 1 1])
-            (permutations [0 0 1 1 1])
-            (permutations [0 1 1 1 1])))
+(defn check [n]
+  (map count
+       (for [m [0 1 2]]
+         (for [x (map #(* n %) (range 10)) :when (< 0 (mod (+ m x) 3))]
+           [m (+ m x)]))))
 
-(def pattern6digits
-  (lazy-cat (permutations [0 0 0 0 0 1])
-            (permutations [0 0 0 0 1 1])
-            (permutations [0 0 0 1 1 1])
-            (permutations [0 0 1 1 1 1])
-            (permutations [0 1 1 1 1 1])))
+(defn digit-repeated-three-times? [ds d]
+  (->> ds
+       (filter #(= d %))
+       count
+       (= 3)))
 
-(def primes5digits
-  (->> primes (drop-while #(< % 10000)) (take-while #(< % 100000))))
+(defn three-times-repeated-digit [n]
+  (let [ds (butlast (digits n))]
+    (cond (digit-repeated-three-times? ds 0) 0
+          (digit-repeated-three-times? ds 1) 1
+          (digit-repeated-three-times? ds 2) 2)))
 
-(def primes6digits
-  (->> primes (drop-while #(< % 100000)) (take-while #(< % 1000000))))
-
-(defn group-key [pattern digits]
-  (->> (map #(if (zero? %1) \* %2) pattern digits)
-       (apply str)))
-
-(defn match? [pattern digits]
-  (->> (map vector pattern digits)
-       (filter (fn [[p _]] (zero? p)))
-       (map last)
-       (apply =)))
-
-(defn p051 [primes patterns]
-  (->> (for [p patterns]
-         (->> (filter #(match? p (digits %)) primes)
-              (group-by #(group-key p (digits %)))))
-       (apply merge)
-       (filter (fn [[_ ps]] (= 8 (count ps))))))
+(defn eight-prime-family? [p rd]
+  (let [p (str p) rd (str rd)]
+    (->> (for [i (range 10)] (s/replace p rd (str i)))
+         (map #(Integer/parseInt %))
+         (filter #(< (count p) (inc (Math/log10 %))))
+         (filter prime?)
+         count
+         (= 8))))
 
 (defn solve []
-  (time (p051 primes5digits pattern5digits))
-  (time (p051 primes6digits pattern6digits)))
+  (->> primes
+       (drop-while #(< % 56003))
+       (map (fn [p] [p (three-times-repeated-digit p)]))
+       (filter second)
+       (filter (fn [[p rd]] (eight-prime-family? p rd)))
+       ffirst))
