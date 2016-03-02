3007c3ba398efc2cee7e0c11e07b07f353db68f2
ntalbs <ntalbsen@gmail.com>
2016-03-02 09:12:42 +0000

    풀이 방식 추가.

--- src/p053.clj
+++ src/p053.clj
@@ -1,11 +1,20 @@
-;; How many, not necessarily distinct, values of C(n,r),
-;; for 1 ≤ n ≤ 100, are greater than one-million?
-
 (ns p053
   (:require [util :refer [factorial]]))
 
+;; (defn c [n r]
+;;   (/ (factorial n) (*' (factorial r) (factorial (- n r)))))
+
+;; (defn c [n r]
+;;   (let [nu (range 1 (inc n))
+;;         de (concat (range 1 (inc r)) (range 1 (inc (- n r))))]
+;;     (->> (map / nu de)
+;;          (reduce *))))
+
 (defn c [n r]
-  (/ (factorial n) (*' (factorial r) (factorial (- n r)))))
+  (let [nu (range (inc (- n r)) (inc n))
+        de (range 1 (inc r))]
+    (->> (map / nu de)
+         (reduce *))))
 
 (defn solve []
   (->> (for [n (range 1 101) r (range 1 101)] (c n r))
