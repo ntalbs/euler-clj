fe079c698a36136b3d59cd26326007cd938e65cf
ntalbs <ntalbsen@gmail.com>
2016-03-05 20:20:33 +0000

    reverse 대신 내림차순 정렬. straight?, flush? 별도 함수로 작성

--- src/p054.clj
+++ src/p054.clj
@@ -4,26 +4,33 @@
   (case c
     \2 2, \3 3, \4 4, \5 5, \6 6, \7 7, \8 8, \9 9, \T 10, \J 11, \Q 12, \K 13, \A 14))
 
-(defn straight? [card-nums]
-  (let [straight (->> "A23456789TJQKA"
-                      (map n->p)
-                      (partition 5 1)
-                      (map set)
-                      set)]
-    (contains? straight (set card-nums))))
+(def straight-set
+  (->> "A23456789TJQKA"
+       (partition 5 1)
+       (map set)
+       set))
+
+(defn straight? [hand]
+  (let [card-nums (->> hand (map first) set)]
+    (contains? straight-set (set card-nums))))
+
+(defn flush? [hand]
+  (= 1 (->> hand (map second) set count)))
+
+(defn desc [x y] (compare y x))
 
 (defn rank [hand]
   (let [ranks [[1 1 1 1 1] [2 1 1 1] [2 2 1] [3 1 1] [] [] [3 2] [4 1]]
-        nums   (->> hand (map first) (map n->p) sort (partition-by identity))
-        r1     (->> nums (map count) sort reverse (.indexOf ranks))
-        r2     (->> nums (sort-by count) reverse (map first) vec)
-        flush? (= 1 (->> hand (map second) set count))
-        str?   (straight? r2)]
+        nums   (->> hand (map first) (map n->p) (sort desc) (partition-by identity))
+        r1     (->> nums (map count) (sort desc) (.indexOf ranks))
+        r2     (->> nums (sort-by count desc) (map first) vec)
+        str?   (straight? hand)
+        flush? (flush? hand)]
     (cond
-      (and flush? (= r2 [14 13 12 11 10])) [9 r2]   ; royal straight flush
-      (and flush? str?) [8  r2]                     ; straight flush
-      flush?            [5  r2]                     ; flush
-      str?              [4  r2]                     ; straight
+      (and flush? (= r2 [14 13 12 11 10])) [9 r2] ; royal straight flush
+      (and flush? str?) [8  r2]                   ; straight flush
+      flush?            [5  r2]                   ; flush
+      str?              [4  r2]                   ; straight
       :else             [r1 r2])))
 
 (def games
