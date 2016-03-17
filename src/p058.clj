b03c87e3d75ccf60a2fb6e09f9a7b8c4e681e94c
ntalbs <ntalbsen@gmail.com>
2016-03-17 22:17:43 +0000

    재구현.

--- src/p058.clj
+++ src/p058.clj
@@ -1,30 +1,25 @@
 (ns p058
   (:require [util :refer (prime?)]))
 
-(defn square [n] (* n n))
+(defn- square [n] (* n n))
 
-(defn l
-  "compute diagonal elements on i-th ring"
-  ([i n] (+ (-> (* 2 i) (- 2) (* n)) (square (-> (* 2 i) (- 3)))))
-  ([i] (map #(l i %) (range 1 5))))
+(defn- d
+  "n번째 ring의 대각선 요소 수를 구하는 함수.
+   d(n,k) = 2n(k+1) + (2n-1)^2
+   n=1,2,3..., k=(0,1,2,3)"
+  ([n k] (+ (* 2 n (inc k)) (square (dec (* 2 n)))))
+  ([n]   (map #(d n %) (range 3))))
 
-(def d (for [i (iterate inc 2)] (l i)))
-
-(defn cnt-prime [coll]
+(defn- cnt-prime [coll]
   (count (filter prime? coll)))
 
-(defn accumulate [acc next]
-  (if (map? acc)
-    {:width (+ 2 (:width acc))
-     :prime (+ (:prime acc) (cnt-prime next))
-     :total (+ (:total acc) (count next))}
-    {:width 3
-     :prime (+ (cnt-prime acc) (cnt-prime next))
-     :total (+ (count acc) (count next))}))
-
 (defn solve []
-  (->> (reductions accumulate {:width 1 :prime 0 :total 1} d)
-       (rest) ;; skip first
-       (map (fn [a] {:width (:width a) :r (/ (:prime a) (:total a))}))
-       (drop-while (fn [m] (>= (:r m) 0.1)))
-       (first)))
+  (->> (iterate inc 1)
+       (map d)
+       (reductions (fn [[w pc tc] ns]
+                     [(+ 2 w) (+ pc (cnt-prime ns)) (+ tc 4)])
+                   [1 0 1])
+       (rest)
+       (map (fn [[w pc tc]] [w (/ pc tc)]))
+       (drop-while (fn [[_ r]] (>= r 0.1)))
+       ffirst))
