b38c8e3404aa37764bd15416e601f9ed23e6ba12
ntalbs <ntalbsen@gmail.com>
2016-04-06 23:40:13 +0100

    코드 정리.

--- src/p061.clj
+++ src/p061.clj
@@ -1,47 +1,46 @@
 (ns p061
   (:require [util :refer (infix)]))
 
-(defn p [k n]
-  (cond (= k 3) (infix ((n * (n + 1)) / 2))
-        (= k 4) (* n n)
-        (= k 5) (infix ((n * ((3 * n) - 1)) / 2))
-        (= k 6) (infix (n * ((2 * n) - 1)))
-        (= k 7) (int (infix (n * (((5 * n) - 3) / 2))))
-        (= k 8) (infix (n * ((3 * n) - 2)))
-        :else (throw (Exception. "invalid index"))))
+(defn- p [k n]
+  (condp = k
+    3 (infix ((n * (n + 1)) / 2))
+    4 (infix (n * n))
+    5 (infix ((n * ((3 * n) - 1)) / 2))
+    6 (infix (n * ((2 * n) - 1)))
+    7 (infix ((n * ((5 * n) - 3)) / 2))
+    8 (infix (n * ((3 * n) - 2)))))
 
-(defn s [k]
+(defn- s [k]
   (->> (iterate inc 1)
        (map #(p k %))
        (drop-while #(< % 1000))
        (take-while #(< % 10000))))
 
-(defn ps [k]
+(defn- ps [k]
   (->> (s k)
-       (map (fn [n] {:g k, :n n}))))
+       (map (fn [n] {:k k, :n n}))))
 
-(defn first-2-digits [n] (quot n 100))
-(defn last-2-digits [n] (rem n 100))
+(defn- first-2-digits [n] (quot n 100))
+(defn- last-2-digits [n] (rem n 100))
 
-(defn chain [M ps]
+(def M
+  (->> (range 3 9)
+       (mapcat ps)
+       (group-by (fn [e] (first-2-digits (e :n))))))
+
+(defn- chain [ps]
   (for [e ps, t (M (last-2-digits (:n (last e))))] (conj e t)))
 
-(defn p061 []
-  (let [M (->> (range 3 9)
-               (map ps)
-               (apply concat)
-               (group-by (fn [e] (first-2-digits (e :n)))))]
-    (->> (chain M (map vector (ps 8)))
-         (chain M)
-         (chain M)
-         (chain M)
-         (chain M)
-         (chain M)
-         (filter #(= (first %) (last %))) ; check cycle
-         (filter (fn [e] (= #{3 4 5 6 7 8} (set (map #(% :g) e)))))
-         first rest
-         (map #(:n %))
-         (apply +))))
-
 (defn solve []
-  (time (p061)))
+  (->> (map vector (ps 8))
+       (chain)
+       (chain)
+       (chain)
+       (chain)
+       (chain)
+       (chain)
+       (filter #(= (first %) (last %))) ; check cycle
+       (filter (fn [e] (= #{3 4 5 6 7 8} (set (map #(% :k) e)))))
+       first rest
+       (map #(:n %))
+       (apply +)))
