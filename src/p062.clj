f147168956ad47d831736d135e0fb67558a3429d
ntalbs <ntalbsen@gmail.com>
2016-04-11 19:58:09 +0100

    재구현.

--- src/p062.clj
+++ src/p062.clj
@@ -1,8 +1,5 @@
-;; Find the smallest cube for which exactly five permutations of its digits are cube.
-
 (ns p062
-  (:require [clojure.contrib.combinatorics :refer (permutations)]
-            [util :refer (digits)]))
+  (:require [util :refer (digits)]))
 
 (def cubics
   (->> (iterate inc 1)
@@ -11,13 +8,12 @@
 (defn sorted-digits [n]
   (sort (digits n)))
 
-(defn p062 [cnt-limit]
-  (->> (take cnt-limit cubics)
-       (group-by #(sorted-digits %))
-       (filter (fn [[k v]] (<= 5 (count v))))
-       (mapcat (fn [[k v]] v))
-       (apply min)))
-
-; check for first 10,000 cubics
 (defn solve []
-  (time (println (p062 10000))))
+  (loop [cs cubics m {}]
+    (let [c  (first cs)           ; current cubic number
+          k  (sorted-digits c)    ; key from c
+          m  (update m k conj c)  ; map
+          cl (m k)]               ; cubic list for k
+      (if (= 5 (count cl))
+        (apply min cl)
+        (recur (rest cs) m)))))
