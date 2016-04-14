15a90242bcb36ad806b430a14395cd6d3148534c
ntalbs <ntalbsen@gmail.com>
2016-04-14 08:25:49 +0100

    재구현.

--- src/p063.clj
+++ src/p063.clj
@@ -1,30 +1,17 @@
-;; How many n-digit positive integers exist which are also an nth power?
-
 (ns p063)
 
 (defn power [x n] (apply *' (repeat n x)))
-(defn count-digit [x] (inc (int (Math/log10 x))))
 
-(defn n-digits-and-nth-power
-  "Returns the sequence of numbers that is n-digit and n-th power simultaneously."
+(def limit (/ 1 (- 1 (Math/log10 9))))
+
+(defn digits&power
+  "Find the numbers which are n-digits and n-th power."
   [n]
-  (->> (iterate inc 1)
-       (map (fn [x] (power x n)))
-       (drop-while (fn [x] (< (count-digit x) n)))
-       (take-while (fn [x] (= (count-digit x) n)))))
-
-(def limit
-  "Max of n, where n-digit and n-power number is possible.
-   This value can be determined by checking where the number of digits of 9^n become less than n."
-  (->> (iterate inc 1)
-       (map #(vector % (count-digit (power 9 %))))
-       (take-while (fn [[n d]] (= n d)))
-       count))
-
-(defn p063 []
-  (->> (range 1 (inc limit))
-       (mapcat n-digits-and-nth-power)
-       count))
+  (for [x (range 1 10)
+        :when (<= (/ (dec n) n) (Math/log10 x))]
+    (power x n)))
 
 (defn solve []
-  (time (println (p063))))
+  (->> (range 1 limit)
+       (mapcat digits&power)
+       count))
