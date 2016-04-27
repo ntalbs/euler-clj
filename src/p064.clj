fd122e726d8f33a6c75993abe920e3379dfa9116
ntalbs <ntalbsen@gmail.com>
2016-04-27 03:45:45 +0100

    코드 정리.

--- src/p064.clj
+++ src/p064.clj
@@ -1,6 +1,3 @@
-;; Continued fraction expansion
-;; http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion
-
 (ns p064)
 
 (defn expand-continued-fraction [n]
@@ -11,14 +8,11 @@
         (let [m (- (* d a) m), d (/ (- n (* m m)) d), a (int (/ (+ a0 m) d))]
           (recur m d a (conj acc a)))))))
 
-(defn sqr [x] (* x x))
+(defn square [x] (* x x))
 
-(defn p064 []
+(defn solve []
   (->> (range 2 (inc 10000))
-       (filter #(not= % (sqr (int (Math/sqrt %)))))
+       (filter #(not= % (square (int (Math/sqrt %)))))
        (map expand-continued-fraction)
        (filter #(even? (count %)))
        count))
-
-(defn solve []
-  (time (p064)))
