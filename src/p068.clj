90624fa8bf9597943f77d3134735b8c12913b950
ntalbs <ntalbsen@gmail.com>
2016-05-24 09:15:06 +0100

    변수 재배치

--- src/p068.clj
+++ src/p068.clj
@@ -1,20 +1,16 @@
 (ns p068
   (:require [clojure.math.combinatorics :refer [permutations]]))
 
-(defn p068 []
+(defn solve []
   (->> (permutations [1 2 3 4 5 6 7 8 9 10])
        (filter (fn [[a b c d e f g h i j]]
                  (= (+ a b c)
-                    (+ j f b)
-                    (+ i e f)
-                    (+ h d e)
-                    (+ g c d))))
-       (filter (fn [[a b c d e f g h i j]]
-                 (= a (min a g h i j))))
-       (map (fn [[a b c d e f g h i j]] (apply str [a b c g c d h d e i e f j f b])))
+                    (+ d c e)
+                    (+ f e g)
+                    (+ h g i)
+                    (+ j i b))))
+       (filter (fn [[a b c d e f g h i j]] (= a (min a d f h j))))
+       (map (fn [[a b c d e f g h i j]] (apply str [a b c d c e f e g h g i j i b])))
        (filter #(= 16 (count %)))
        (map #(Long/parseLong %))
        (apply max)))
-
-(defn solve []
-  (time (p068)))
