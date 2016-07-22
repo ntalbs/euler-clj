9f69e4d24e487d915803ecb3543443043e9096fa
ntalbs <ntalbsen@gmail.com>
2016-07-22 07:51:42 +0100

    풀이 방법 추가.

--- src/p072.clj
+++ src/p072.clj
@@ -3,6 +3,8 @@
 
 (def limit 1000000)
 
+
+;; first try
 (defn phi
   ([p k]
    (* (pow p k) (- 1 (/ 1 p))))
@@ -16,6 +18,7 @@
        (pmap phi)
        (apply +)))
 
+;; second try
 (defn- asum [^ints xs]
   (areduce xs i ret (long 0) (+ ret (aget xs i))))
 
@@ -30,3 +33,18 @@
       (if (< i limit)
         (recur (inc i))))
     (asum phi)))
+
+
+;; thrid try
+(defn solve3 []
+  (let [limit 1000000
+        phi (int-array (range (inc limit)))]
+    (loop [i 2 acc 0]
+      (if (= i (aget phi i))
+        (loop [j i]
+          (if (<= j limit)
+            (do (aset-int phi j (/ (* (aget phi j) (dec i)) i))
+                (recur (+ j i))))))
+      (if (< i limit)
+        (recur (inc i) (+ acc (aget phi i)))
+        acc))))
