c00610cbcb9ad1dea4eafcb62ad8e25141cdb3c8
ntalbs <ntalbsen@gmail.com>
2016-08-02 15:30:03 +0100

    코드 정리 + 새 풀이 추가.

--- src/p073.clj
+++ src/p073.clj
@@ -1,11 +1,35 @@
 (ns p073
-  (:require [util :refer (gcd)]))
+  (:require [util :refer [gcd infix]]))
 
+;; brute force
 (defn reduced-proper-fractions [limit]
-  (for [d (range 3 (inc limit)) n (range (int (/ d 3)) (inc (int (/ d 2))))
-        :when (< 1/3 (/ n d) 1/2)
+  (for [d (range 3 (inc limit))
+        n (range (int (/ d 3)) (inc (int (/ d 2))))
+        :when (< 1/3 (/ (double n) d) 1/2)
         :when (= 1 (gcd n d))]
     (/ n d)))
 
-(defn solve []
+(defn solve1 []
   (count (reduced-proper-fractions 12000)))
+
+;; farey sequence
+(defn- next-to [a b limit]
+  (->> (range 2 limit)                                ; d
+       (map (fn [d] [(infix (((a * d) + 1) / b)) d])) ; [c d]
+       (filter (fn [[c d]] (integer? c)))
+       (map (fn [[c d]] (/ c d)))
+       (apply min)))
+
+;; http://blog.mishkovskyi.net/posts/2015/Oct/29/clojure-numbers-despair
+(defn solve2 []
+  (let [limit 12000
+        a 1
+        b 3
+        r (next-to a b limit)
+        c (numerator r)
+        d (denominator r)]
+    (loop [a a, b b, c (int c), d (int d), acc 0]
+      (if (< (/ (double c) d) 0.5)
+        (let [k (quot (+ limit b) d)]
+          (recur c d (- (* k c) a) (- (* k d) b) (inc acc)))
+        acc))))
