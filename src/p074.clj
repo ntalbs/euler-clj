5671a21e9c09b596ea44f88daecae0b0aa46e6dc
ntalbs <ntalbsen@gmail.com>
2016-08-18 22:31:27 +0100

    풀이 개선.

--- src/p074.clj
+++ src/p074.clj
@@ -1,24 +1,49 @@
 (ns p074
-  (:require [util :refer (digits factorial)]))
+  (:require [util :refer [digits]]))
 
-(defn sum-of-factorial [n]
+(defn- fact [n]
+  (case n
+    0 1
+    1 1
+    2 2
+    3 6
+    4 24
+    5 120
+    6 720
+    7 5040
+    8 40320
+    9 362880))
+
+(defn- fact-sum [n]
   (->> (digits n)
-       (map factorial)
+       (map fact)
        (apply +)))
 
-(def sum-of-factorial (memoize sum-of-factorial))
-
-(defn count-steps [n]
+(defn- chain-length [n]
   (loop [s n chain #{n} cnt 1]
-    (let [x (sum-of-factorial s)]
-      (if (contains? chain x) cnt
-          (recur x (conj chain x) (inc cnt))))))
+    (let [x (fact-sum s)]
+      (if (contains? chain x)
+        cnt
+        (recur x (conj chain x) (inc cnt))))))
 
-(defn p074 []
+;; first approach. slow.
+(defn solve1 []
   (->> (range 1 (inc 1000000))
-       (filter #(= 60 (count-steps %)))
+       (filter #(= 60 (chain-length %)))
        count))
 
-; Takes about 25 secs. Need to be improved.
-(defn solve []
-  (time (println (p074))))
+;; second approach.
+(defn- sorted-digits-num [n]
+  (->> (digits n)
+       (sort >)
+       (apply str)
+       (Integer/parseInt)))
+
+(defn solve2 []
+  (->> (range 3 (inc 1000000))
+       (map sorted-digits-num)
+       frequencies
+       (map (fn [[k cnt]] [(chain-length k) cnt]))
+       (filter (fn [[k _]] (= k 60)))
+       (map second)
+       (apply +)))
