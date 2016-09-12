b5c6e0cfe3c7d413a57d0f6a86a8a94d3d130f49
ntalbs <ntalbsen@gmail.com>
2016-09-12 22:45:16 +0100

    코드 정리

--- src/p076.clj
+++ src/p076.clj
@@ -7,14 +7,11 @@
 (defn p [n k]
   (cond (= k 0) 0
         (= n 0) 1
-        (= n 1) 1                     ; (p 1 n) always goes to 1
+        (= n 1) 1                     ; (p 1 k) always goes to 1
         (< n 0) 0
         :else (+ (p n (dec k)) (p (- n k) k))))
 
 (def p (memoize p))
 
-(defn p076 []
-  (p 100 99))
-
 (defn solve []
-  (time (println (p076))))
+  (p 100 99))
