265663031604f111b879ba47827a3415baef3440
ntalbs <ntalbsen@gmail.com>
2016-12-22 23:48:21 +0000

    priority-map 추가

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/core.logic "0.8.10"]]
+                 [org.clojure/core.logic "0.8.10"]
+                 [org.clojure/data.priority-map "0.0.7"]]
   :jvm-opts ["-XX:+UnlockCommercialFeatures" "-XX:+FlightRecorder" "-XX:FlightRecorderOptions=defaultrecording=true"]
 )
