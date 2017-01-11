b6386e37531b67cce51bc6686db2eb697cc0680d
ntalbs <ntalbsen@gmail.com>
2017-01-11 23:41:00 +0000

    코드 정리

--- src/p082.clj
+++ src/p082.clj
@@ -6,18 +6,18 @@
        (split  #"\r\n")
        (mapv (fn [line] (mapv parse-int (split #"," line ))))))
 
-(defn cost [vs [x y]]
+(defn min-sum [vs [x y]]
   (if (empty? vs)
     (conj vs (+ x y))
     (conj vs (min (+ x y) (+ y (last vs))))))
 
-(defn sweep-row [xs ys]                 ; from top to bottom
-  (let [cs1 (reduce cost [] (map vector xs ys))
-        cs2 (reverse (reduce cost [] (map vector (reverse xs) (reverse ys))))]
+(defn sweep [xs ys]
+  (let [cs1 (reduce min-sum [] (map vector xs ys))
+        cs2 (reverse (reduce min-sum [] (reverse (map vector xs ys))))]
     (map min cs1 cs2)))
 
-(defn solve [m]
+(defn solve []
   (->> (apply mapv vector m)            ; transpose
-       (reverse)
-       (reduce sweep-row)
+       ;; (reverse)
+       (reduce sweep)
        (apply min)))
