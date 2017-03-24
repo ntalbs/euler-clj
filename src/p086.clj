074cf04bfc09e1356ea35f1e71280433fc2da1cd
ntalbs <ntalbsen@gmail.com>
2017-03-24 18:59:58 +0000

    코드 정리

--- src/p086.clj
+++ src/p086.clj
@@ -1,19 +1,19 @@
 (ns p086
   (:require [util :refer [perfect-square?]]))
 
-(defn cnt [M]
-  (apply +
-         (for [bc (range 3 (inc (* 2 M))) :when (perfect-square? (+ (* M M) (* bc bc)))]
-           (if (<= bc M)
-             (int (/ bc 2))
-             (inc (- M (int (/ (+ bc 1) 2))))))))
+(def ^:private threshold 1000000)
 
-(defn p086 [limit]
+(defn cnt [c]
+  (apply +
+         (for [l (range 3 (inc (* 2 c))) ; l <= a+b
+               :when (perfect-square? (+ (* c c) (* l l)))]
+           (if (<= l c)
+             (quot l 2)
+             (- c (quot (- l 1) 2))))))
+
+(defn solve []
   (->> (iterate inc 3)
        (map (fn [x] [x (cnt x)]))
        (reductions (fn [[a1 a2] [b1 b2]] [b1 (+ a2 b2)]))
-       (drop-while (fn [[M cnt]](<= cnt limit)))
-       first))
-
-(defn solve []
-  (time (println (p086 1000000))))
+       (drop-while (fn [[M cnt]] (<= cnt threshold)))
+       ffirst))
