45acf3a44748ebd865018b6e4d6c968a95e0f727
ntalbs <ntalbsen@gmail.com>
2017-07-07 14:08:29 +0100

    Extract function

--- src/p087.clj
+++ src/p087.clj
@@ -1,15 +1,18 @@
 (ns p087
   (:require [clojure.contrib.lazy-seqs :refer [primes]]))
 
-(defn calc [a b c]
+(defn- calc [a b c]
   (+ (* a a) (* b b b) (* c c c c)))
 
-(def ps1 (take-while #(< % 7071) primes)) ; pow(50,000,000, 1/2)
-(def ps2 (take-while #(< %  368) primes)) ; pow(50,000,000, 1/3)
-(def ps3 (take-while #(< %   84) primes)) ; pow(50,000,000, 1/4)
+(defn- nth-root [n x]
+  (int (Math/pow x (/ 1.0 n))))
+
+(def ps1 (take-while #(< % (nth-root 2 50000000)) primes))
+(def ps2 (take-while #(< % (nth-root 3 50000000)) primes))
+(def ps3 (take-while #(< % (nth-root 4 50000000)) primes))
 
 (defn solve []
   (->> (for [a ps1, b ps2, c ps3] (calc a b c))
        (filter #(< % 50000000))
-       distinct
+       set
        count))
