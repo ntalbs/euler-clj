69491894ba4c43f4b51fbce47bb6414dbde70c8c
ntalbs <ntalbsen@gmail.com>
2017-11-06 19:29:11 +0000

    코드 정리

--- src/p091.clj
+++ src/p091.clj
@@ -10,8 +10,8 @@
 
 (def triangles
   (for [p1 points p2 points
-        :when (not= p1 p2) ;; exclude when p1 and p2 are the same point
-        :when (not= (e p1) (e p2))] ;; exclude when p1 and p2 are on the straight line
+        :when (not= p1 p2)          ; exclude when p1 and p2 are the same point
+        :when (not= (e p1) (e p2))] ; exclude when p1 and p2 are on the straight line
     [p1 p2]))
 
 (defn inner-product [[a1 a2] [b1 b2]]
@@ -21,13 +21,12 @@
   [(- a1 b1) (- a2 b2)])
 
 (def right-triangles
-  (for [[A B] triangles
-        :when (or (= 0 (inner-product A B))
-                  (= 0 (inner-product A (subtract B A)))
-                  (= 0 (inner-product B (subtract B A))))]
+  (for [[P Q] triangles
+        :when (or (= 0 (inner-product P Q))
+                  (= 0 (inner-product P (subtract Q P)))
+                  (= 0 (inner-product Q (subtract Q P))))]
     [A B]))
 
-; p1, p2가 위치만 바뀐 경우가 있으므로 2로 나눠야 함
-; XXX: p1, p2 생성 시 위치만 바뀐 것은 생성되지 않도록 할 수는 없을까?
+; P, Q가 위치만 바뀐 경우가 있으므로 2로 나눠야 함
 (defn solve []
   (/ (count right-triangles) 2))
