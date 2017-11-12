9e375c995634bdd59f2061572d015aabc5c25144
ntalbs <ntalbsen@gmail.com>
2017-11-12 20:02:27 +0000

    재구현

--- src/p091.clj
+++ src/p091.clj
@@ -3,30 +3,20 @@
 (def LIMIT 50)
 
 (def points
-  (for [x (range (inc LIMIT)) y (range (inc LIMIT)) :when (not (= 0 x y))]
+  (for [x (range 1 (inc LIMIT)) y (range 1 (inc LIMIT))]
     [x y]))
 
-(defn e [[x y]] (if (= y 0) nil (/ x y)))
+(defn fn-line-perpendicular-to [[a b]]
+  (fn [x] (+ (/ (* (- a) x) b) b (/ (* a a) b))))
 
-(def triangles
-  (for [p1 points p2 points
-        :when (not= p1 p2)          ; exclude when p1 and p2 are the same point
-        :when (not= (e p1) (e p2))] ; exclude when p1 and p2 are on the straight line
-    [p1 p2]))
-
-(defn inner-product [[a1 a2] [b1 b2]]
-  (+ (* a1 b1) (* a2 b2)))
-
-(defn subtract [[a1 a2] [b1 b2]]
-  [(- a1 b1) (- a2 b2)])
-
-(def right-triangles
-  (for [[P Q] triangles
-        :when (or (= 0 (inner-product P Q))
-                  (= 0 (inner-product P (subtract Q P)))
-                  (= 0 (inner-product Q (subtract Q P))))]
-    [A B]))
-
-; P, Q가 위치만 바뀐 경우가 있으므로 2로 나눠야 함
 (defn solve []
-  (/ (count right-triangles) 2))
+  (->> points
+       (map fn-line-perpendicular-to)
+       (mapcat (fn [f] (->> (range (inc LIMIT))
+                            (map f)
+                            (filter integer?)
+                            (filter #(<= 0 % 50)))))
+       count
+       (+ 2500)   ; P, Q가 각각 x축, y축에 있는 경우
+       (+ 2500)   ; P는 x축, Q는 1사분면 안에 있는 경우
+       (+ 2500))) ; P는 y축, Q는 1사분면 안에 있는 경우
