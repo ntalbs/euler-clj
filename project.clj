749f385c0dac108795c9de58a7385f00f6d53e7a
ntalbs <ntalbsen@gmail.com>
2017-12-22 13:49:41 +0000

    Update dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler "0.1.0-SNAPSHOT"
   :description "Project Euler solutions in Clojure"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/core.logic "0.8.10"]
