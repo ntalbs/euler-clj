bfa31318938e87b33709756148ce93695e41349d
ntalbs <ntalbsen@gmail.com>
2017-12-22 13:48:31 +0000

    순열 조합을 이용한 풀이 추가

--- src/p092.clj
+++ src/p092.clj
@@ -1,17 +1,48 @@
 (ns p092
-  (:require [util :refer [digits]]))
+  (:require [util :refer [digits factorial]]))
 
-(defn sum-sq-digits [n]
+;; Brute-force
+(defn- sum-sq-digits [n]
   (->> (digits n)
        (map #(* % %))
        (reduce +)))
 
-(defn reach89? [n]
+(defn- reach89? [n]
   (cond (= n 1) false
         (= n 89) true
-        :else (recur (sum-sq-digits n))))
+        :else (reach89? (sum-sq-digits n))))
 
-(defn solve []
+(def reach89? (memoize reach89?))
+
+(defn solve1 []
   (->> (range 1 10000000)
        (filter reach89?)
        (count)))
+
+;; Combinatorics approach
+(defn- cnt [ds]
+  (->> ds
+       (group-by identity)
+       (map (fn [[_ d]] (count d)))
+       (map factorial)
+       (apply *)
+       (quot (factorial 7))))
+
+(defn- vn [digits]
+  (reduce (fn [a b] (+ (* 10 a) b)) digits))
+
+(defn solve2 []
+  (let [r (range 0 10)]
+    (->> (for [a r
+               b r
+               c r
+               d r
+               e r
+               f r
+               g r
+               :when (<= a b c d e f g)
+               :when (not= 0 a b c d e f g)] [a b c d e f g])
+         (map (fn [ds] [(vn ds) (cnt ds)]))
+         (filter (fn [[k cnt]] (reach89? k)))
+         (map (fn [[k cnt]] cnt))
+         (apply +))))
