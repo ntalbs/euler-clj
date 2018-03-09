78ce8ecbf0beaf0432f09179c609d9963061b78b
ntalbs <ntalbsen@gmail.com>
2018-03-09 13:47:37 +0000

    Solution of #88.

---
+++ src/p088.clj
@@ -0,0 +1,22 @@
+;; https://blog.dreamshire.com/project-euler-88-solution/
+
+(ns p088)
+
+(def kmax 12000)
+
+(defn- prodsum [p s c start n]
+  (let [k (+ (- p s) c)]
+    (if (< k kmax)
+      (do
+        (if (< p (@n k))
+          (swap! n assoc k p))
+        (doseq [i (range start (inc (* (quot kmax p) 2)))]
+          (prodsum (* p i) (+ s i) (+ c 1) i n))))))
+
+(defn solve []
+  (let [n (atom (vec (repeat kmax (* kmax 2))))]
+    (prodsum 1 1 1 2 n)
+    (->> @n
+         (drop 2)
+         (distinct)
+         (apply +))))
