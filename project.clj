01eb72e54ec5b0b59dcf8a755134ac305cb3db69
ntalbs <ntalbsen@gmail.com>
2018-08-31 00:21:07 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/core.logic "0.8.10"]
+                 [org.clojure/core.logic "0.8.11"]
                  [org.clojure/data.priority-map "0.0.7"]]
 )
