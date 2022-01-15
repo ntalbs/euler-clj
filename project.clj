767a74ec4028c67019949ba012c67535c4e4a7a8
Seongjun Yun <seongjun@amazon.com>
2022-01-15 12:02:32 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject euler "0.1.0-SNAPSHOT"
   :description "Project Euler solutions in Clojure"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/math.combinatorics "0.1.1"]
-                 [org.clojure/core.logic "0.8.11"]
-                 [org.clojure/data.priority-map "0.0.7"]]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/core.logic "1.0.0"]
+                 [org.clojure/data.priority-map "1.1.0"]]
 )
